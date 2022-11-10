d2c9de8c136510d533c7910c9d9833d8421cc3e6
kumchovylcho <tamerchoyy@gmail.com>
2022-11-10 10:08:52 +0200

    fixed number validation throwing errors

--- extra_options/quantity_and_price_validation.py
+++ extra_options/quantity_and_price_validation.py
@@ -1,13 +1,12 @@
 def check_for_number(number):
     """
     param number: Is the number that is being checked.
-    This function checks if the quantity is integer or float.
+    This function checks if the number is integer or float.
     return: Returns integer or float number.
     """
     allowed_characters = "0123456789."
-    scan_all_symbols = [digit if digit in allowed_characters else digit for digit in number]
-    check_for_letter_or_symbol = [digit for digit in scan_all_symbols if digit not in allowed_characters]
-    if scan_all_symbols and 0 < scan_all_symbols.count(".") < 2 and not check_for_letter_or_symbol:
+    check_for_letter_or_symbol = [digit for digit in number if digit not in allowed_characters]
+    if len(number) > 1 and 0 < number.count(".") < 2 and not check_for_letter_or_symbol:
         return float(number)
-    elif scan_all_symbols and not scan_all_symbols.count(".") and not check_for_letter_or_symbol:
+    elif number and not number.count(".") and not check_for_letter_or_symbol:
         return int(number)
